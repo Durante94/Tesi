025acbd2a7d4b7bc20e83da06afd27cd79e15111
Durante94 <fabrizio.durante294@gmail.com>
2023-04-26 21:20:40 +0000

    feat/config/db config

---
+++ components/microservices/configuration-scheduler/src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
+
+CREATE TABLE input_entities (
+    Id bigint NOT NULL AUTO_INCREMENT,
+    name varchar(50) NOT NULL,
+    description varchar(100) DEFAULT NULL,
+    amplitude decimal(6,3) NOT NULL DEFAULT 0,
+    frequency decimal(6,3) NOT NULL DEFAULT 0,
+    function varchar(10) NOT NULL,
+    enable tinyint(1) NOT NULL DEFAULT 0
+);
