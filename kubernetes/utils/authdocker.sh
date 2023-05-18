be888d998e9211f83fa95a15a677145d21b9f45b
fabrizio.durante <fabrizio.durante@algowatt.com>
2023-05-18 19:03:43 +0200

    feat/k8s/image pull and gateway route fix

--- kubernetes/utils/authdocker.sh
+++ kubernetes/utils/authdocker.sh
@@ -5,7 +5,7 @@ set +e
 export DOCKER_REGISTRY_SERVER=https://index.docker.io/v1/ ; 
 export DOCKER_USER=fabrizio294 ;
 export DOCKER_EMAIL=fabrizio.durante294@gmail.com ;
-export DOCKER_PASSWORD='a45W[=nw3y3Fp6>!' ; 
+export DOCKER_PASSWORD='dckr_pat_aeWHKHK8FgAopinT4G3E4UoY0eY' ; 
 
 k0s kubectl create secret docker-registry cfcr --docker-server=$DOCKER_REGISTRY_SERVER --docker-username=$DOCKER_USER --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL -n backend; 
 k0s kubectl create secret docker-registry cfcr --docker-server=$DOCKER_REGISTRY_SERVER --docker-username=$DOCKER_USER --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL -n frontend; 
