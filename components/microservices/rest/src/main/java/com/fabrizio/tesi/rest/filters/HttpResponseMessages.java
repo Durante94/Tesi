039f06d1a3bb81f75e9a05eadb8fb1ea8fd8b3a7
Durante94 <fabrizio.durante294@gmail.com>
2023-05-22 21:09:27 +0000

    feat/rest/auth filter

---
+++ components/microservices/rest/src/main/java/com/fabrizio/tesi/rest/filters/HttpResponseMessages.java
@@ -0,0 +1,21 @@
+package com.fabrizio.tesi.rest.filters;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HttpResponseMessages {
+
+    public void returnAuthError(HttpServletResponse response) {
+
+        try {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Utente non autorizzato");
+        } catch (IOException e) {
+            log.error("Response error", e);
+        }
+    }
+
+}
