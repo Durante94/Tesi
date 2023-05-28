5ca01d9c65d4ea075bc94629eaba62556f748dae
Durante94 <fabrizio.durante294@gmail.com>
2023-05-28 14:44:24 +0000

    feat/socket/auth filter

--- components/microservices/socket/src/main/java/com/fabrizio/tesi/socket/config/WebSocketConfig.java
+++ components/microservices/socket/src/main/java/com/fabrizio/tesi/socket/config/WebSocketConfig.java
@@ -1,14 +1,19 @@
 package com.fabrizio.tesi.socket.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.fabrizio.tesi.socket.filters.AuthFilter;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private static final String endpont = "ws-message";
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -18,7 +23,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-message").withSockJS();
+        registry.addEndpoint(String.format("/%s", endpont)).withSockJS();
     }
 
+    @Bean
+    FilterRegistrationBean<AuthFilter> authFilter() {
+        FilterRegistrationBean<AuthFilter> bean = new FilterRegistrationBean<>();
+
+        bean.setFilter(new AuthFilter());
+        bean.addUrlPatterns(String.format("/%s/*", endpont));
+
+        return bean;
+    }
 }
---
+++ components/microservices/socket/src/main/java/com/fabrizio/tesi/socket/filters/AuthFilter.java
@@ -0,0 +1,89 @@
+package com.fabrizio.tesi.socket.filters;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+@Order(1)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Slf4j
+public class AuthFilter extends OncePerRequestFilter {
+    @Value("${auth.secret}")
+    String secret;
+
+    @Value("${auth.enable}")
+    Boolean authEnabled;
+
+    @Value("${auth.mockRole}")
+    String mockRole;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        MutableHttpServletRequest mutableRequest = new MutableHttpServletRequest(request);
+
+        if (!authEnabled) {
+            mutableRequest.putHeader("role", mockRole);
+            try {
+                filterChain.doFilter(mutableRequest, response);
+            } catch (IOException | ServletException e) {
+                log.error("Filter error:", e);
+            }
+            return;
+        }
+        String token = request.getHeader("Authorization");
+
+        if (token == null) {
+            new HttpResponseMessages().returnAuthError(response);
+            return;
+        }
+
+        Algorithm algorithm = null;
+        try {
+            algorithm = Algorithm.HMAC256(secret);
+        } catch (IllegalArgumentException | UnsupportedEncodingException e1) {
+            log.error("Token problems", e1);
+            new HttpResponseMessages().returnAuthError(response);
+            return;
+        }
+
+        DecodedJWT jwt = null;
+
+        try {
+            JWTVerifier verifier = JWT.require(algorithm).build(); // Reusable verifier instance
+            jwt = verifier.verify(token);
+        } catch (JWTVerificationException exception) {
+            log.error("JWT", exception);
+            new HttpResponseMessages().returnAuthError(response);
+            return;
+        }
+
+        mutableRequest.putHeader("role", jwt.getClaim("role").asString());
+        mutableRequest.putHeader("userName", jwt.getClaim("preferred_username").asString());
+
+        try {
+            filterChain.doFilter(mutableRequest, response);
+        } catch (IOException | ServletException e) {
+            log.error("Cannot elaborate request", e);
+        }
+    }
+}
