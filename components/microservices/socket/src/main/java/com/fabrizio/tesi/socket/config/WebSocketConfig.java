663e463c771bb3aa35348a7f24cff9175e397726
fabrizio.durante <fabrizio.durante@algowatt.com>
2023-05-31 23:20:03 +0200

    feat/socket/cleaning

--- components/microservices/socket/src/main/java/com/fabrizio/tesi/socket/config/WebSocketConfig.java
+++ components/microservices/socket/src/main/java/com/fabrizio/tesi/socket/config/WebSocketConfig.java
@@ -1,14 +1,10 @@
 package com.fabrizio.tesi.socket.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import com.fabrizio.tesi.socket.filters.AuthFilter;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -25,14 +21,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint(String.format("/%s", endpont)).withSockJS();
     }
-
-    // @Bean
-    // FilterRegistrationBean<AuthFilter> authFilter() {
-    //     FilterRegistrationBean<AuthFilter> bean = new FilterRegistrationBean<>();
-
-    //     bean.setFilter(new AuthFilter());
-    //     bean.addUrlPatterns(String.format("/%s/*", endpont));
-
-    //     return bean;
-    // }
 }
