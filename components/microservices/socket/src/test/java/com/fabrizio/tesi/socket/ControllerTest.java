0882f3d5640dbc4b8c36d82db85b94ce5808d7cb
Fabrizio Durante <fabrizio294.durante@outlook.com>
2023-06-22 23:17:45 +0200

    feat/socket/test

--- components/microservices/socket/src/test/java/com/fabrizio/tesi/socket/ControllerTest.java
+++ components/microservices/socket/src/test/java/com/fabrizio/tesi/socket/ControllerTest.java
@@ -1,8 +1,7 @@
 package com.fabrizio.tesi.socket;
 
 import com.fabrizio.tesi.socket.controller.Controller;
-import com.fabrizio.tesi.socket.dto.ConfigRespDTO;
-import com.fabrizio.tesi.socket.dto.ConfigRespPayload;
+import com.fabrizio.tesi.socket.dto.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -10,6 +9,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,6 +20,9 @@ public class ControllerTest {
     @MockBean
     SimpMessagingTemplate socketTemplate;
 
+    @MockBean
+    KafkaTemplate<String, ConfigReqDTO> kafkaTemplate;
+
     @Autowired
     Controller controller;
 
@@ -28,7 +31,7 @@ public class ControllerTest {
     @Test
     void sendConfigTest() throws JsonProcessingException {
         int amplitude = 51, frequency = 28;
-        String function = "tan", headervalue = "test";
+        String function = "tan", headerValue = "test";
         String json = String.format("{\n" +
                 "\t\"amplitude\": %d,\n" +
                 "\t\"frequency\": %d,\n" +
@@ -48,6 +51,73 @@ public class ControllerTest {
             return null;
         }).when(socketTemplate).convertAndSend(Mockito.anyString(), Mockito.any(ConfigRespDTO.class));
 
-        controller.sendConfig(objectMapper.readValue(json, ConfigRespPayload.class), headervalue);
+        controller.sendConfig(objectMapper.readValue(json, ConfigRespPayload.class), headerValue);
+    }
+
+    @Test
+    void sendAlarmTest() throws JsonProcessingException {
+        long time = 1687466895000L, lastHB = 1687466895000L;
+        String type = "testType", headerValue = "test";
+        String json = String.format("{\n" +
+                "\t\"type\": \"%s\",\n" +
+                "\t\"time\": %s,\n" +
+                "\t\"lastHB\": %s\n" +
+                "}", type, Long.toString(time), Long.toString(lastHB));
+
+        Mockito.doAnswer(invocation -> {
+            String arg0 = invocation.getArgument(0, String.class);
+            AlarmDTO arg1 = invocation.getArgument(1, AlarmDTO.class);
+
+            assertEquals(arg0, "/topic/alarm");
+            assertNotNull(arg1);
+            assertNotNull(arg1.getPayload());
+            assertEquals(time, arg1.getPayload().getTime().getTime());
+            assertEquals(lastHB, arg1.getPayload().getLastHB().getTime());
+            assertEquals(type, arg1.getPayload().getType());
+            return null;
+        }).when(socketTemplate).convertAndSend(Mockito.anyString(), Mockito.any(AlarmDTO.class));
+
+        controller.sendAlarm(headerValue, objectMapper.readValue(json, AlarmPayload.class));
+    }
+
+    @Test
+    void broadcastConfigTest() throws JsonProcessingException {
+        int amplitude = 51, frequency = 28;
+        String function = "tan";
+        String json = String.format("{\n" +
+                "\t\"amplitude\": %d,\n" +
+                "\t\"frequency\": %d,\n" +
+                "\t\"function\": \"%s\"\n" +
+                "}", amplitude, frequency, function);
+        ConfigRespPayload payload = new ConfigRespPayload();
+
+        ConfigRespDTO test = controller.broadcastConfig(new ConfigRespDTO(objectMapper.readValue(json, ConfigRespPayload.class)));
+
+        assertNotNull(test);
+        assertEquals("config-resp", test.getType());
+        assertNotNull(test.getPayload());
+        assertEquals(amplitude, test.getPayload().getAmplitude());
+        assertEquals(frequency, test.getPayload().getFrequency());
+        assertEquals(function, test.getPayload().getFunction());
+    }
+
+    @Test
+    void broadcastAlarmTest() throws JsonProcessingException {
+        long time = 1687466895000L, lastHB = 1687466895000L;
+        String type = "testType";
+        String json = String.format("{\n" +
+                "\t\"type\": \"%s\",\n" +
+                "\t\"time\": %d,\n" +
+                "\t\"lastHB\": %d\n" +
+                "}", type, time, lastHB);
+
+        AlarmDTO test = controller.broadcastAlarm(new AlarmDTO(objectMapper.readValue(json, AlarmPayload.class)));
+
+        assertNotNull(test);
+        assertEquals("alarm", test.getType());
+        assertNotNull(test.getPayload());
+        assertEquals(time, test.getPayload().getTime().getTime());
+        assertEquals(lastHB, test.getPayload().getLastHB().getTime());
+        assertEquals(type, test.getPayload().getType());
     }
 }
