2c107ff999c7938109be8545d234c576cc2d09db
Fabrizio Durante <fabrizio294.durante@outlook.com>
2023-06-23 23:52:39 +0200

    feat/rest/test filter

--- components/microservices/rest/src/test/java/com/fabrizio/tesi/rest/AuthFiltersTest.java
+++ components/microservices/rest/src/test/java/com/fabrizio/tesi/rest/AuthFiltersTest.java
@@ -3,9 +3,14 @@ package com.fabrizio.tesi.rest;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import com.fabrizio.tesi.rest.filters.MutableHttpServletRequest;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockFilterChain;
@@ -15,20 +20,46 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.fabrizio.tesi.rest.filters.AuthFilter;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @AutoConfigureTestDatabase
 @ActiveProfiles("dev")
 class AuthFiltersTest {
+	@Value("${auth.mockRole}")
+	String mockRole;
 	@Autowired
 	AuthFilter filter;
 
 	@Test
-	void test() throws ServletException, IOException {
+	void emptyHeaderTest() throws ServletException, IOException {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		MockFilterChain chain = new MockFilterChain();
 
 		filter.doFilter(request, response, chain);
+
+		assertTrue(chain.getRequest() instanceof MutableHttpServletRequest);
+
+		MutableHttpServletRequest customRequest = (MutableHttpServletRequest) chain.getRequest();
+
+		assertNotNull(customRequest.getHeader("role"));
+		assertEquals(mockRole, customRequest.getHeader("role"));
 	}
 
+	@Test
+	void emptyHeaderWithChainErrorTest() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		MockFilterChain chain = Mockito.mock(MockFilterChain.class);
+
+		Mockito.doThrow(new IOException())
+				.when(chain)
+				.doFilter(Mockito.any(ServletRequest.class), Mockito.any(HttpServletResponse.class));
+
+		filter.doFilter(request, response, chain);
+
+
+		assertFalse(chain.getRequest() instanceof MutableHttpServletRequest);
+	}
 }
--- components/microservices/rest/src/test/java/com/fabrizio/tesi/rest/AuthFiltersTestAuthEnable.java
+++ components/microservices/rest/src/test/java/com/fabrizio/tesi/rest/AuthFiltersTestAuthEnable.java
@@ -1,32 +1,34 @@
 package com.fabrizio.tesi.rest;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fabrizio.tesi.rest.filters.AuthFilter;
+import com.fabrizio.tesi.rest.filters.MutableHttpServletRequest;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ActiveProfiles;
 
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fabrizio.tesi.rest.filters.AuthFilter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@SpringBootTest(properties = { "auth.enable=true" })
-@AutoConfigureTestDatabase
-@ActiveProfiles("dev")
-class AuthFiltersTestAuthEnable {
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(properties = {"auth.enable=true", "auth.secret=secret"})
+public class AuthFiltersTestAuthEnable {
+	@Value("${auth.secret}")
+	String secret;
 	@Autowired
 	AuthFilter filter;
 
 	@Test
-	void emptyHeader() throws ServletException, IOException {
+	void noAuthHeaderTest() throws ServletException, IOException {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		MockFilterChain chain = new MockFilterChain();
@@ -35,7 +37,7 @@ class AuthFiltersTestAuthEnable {
 	}
 
 	@Test
-	void invalidToken() throws ServletException, IOException {
+	void emptyAuthHeaderTest() throws ServletException, IOException {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		MockFilterChain chain = new MockFilterChain();
@@ -45,18 +47,72 @@ class AuthFiltersTestAuthEnable {
 		filter.doFilter(request, response, chain);
 	}
 
-//	@Test
-	void nullSecret() throws ServletException, IOException {
+	@Test
+	void validAuthHeaderNoDataTest() throws ServletException, IOException {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		MockFilterChain chain = new MockFilterChain();
 
-		request.addHeader("Authorization", "");
+		Algorithm algorithm = Algorithm.HMAC256(secret);
+		String token = JWT.create().sign(algorithm);
+		request.addHeader("Authorization", token);
 
-		try (MockedStatic<Algorithm> utilities = Mockito.mockStatic(Algorithm.class)) {
-			utilities.when(() -> Algorithm.HMAC256(Mockito.anyString())).thenThrow(IllegalArgumentException.class);
+		filter.doFilter(request, response, chain);
 
-			filter.doFilter(request, response, chain);
-		}
+		assertTrue(chain.getRequest() instanceof MutableHttpServletRequest);
+
+		MutableHttpServletRequest customRequest = (MutableHttpServletRequest) chain.getRequest();
+
+		assertNull(customRequest.getHeader("role"));
+		assertNull(customRequest.getHeader("userName"));
+	}
+
+	@Test
+	void validAuthHeaderWithDataTest() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		MockFilterChain chain = new MockFilterChain();
+		String mockRole = "ruolo", mockUserName = "user";
+
+		Algorithm algorithm = Algorithm.HMAC256(secret);
+		String token = JWT.create()
+				.withClaim("role", mockRole)
+				.withClaim("preferred_username", mockUserName)
+				.sign(algorithm);
+		request.addHeader("Authorization", token);
+
+		filter.doFilter(request, response, chain);
+
+		assertTrue(chain.getRequest() instanceof MutableHttpServletRequest);
+
+		MutableHttpServletRequest customRequest = (MutableHttpServletRequest) chain.getRequest();
+
+		assertNotNull(customRequest.getHeader("role"));
+		assertNotNull(customRequest.getHeader("userName"));
+		assertEquals(mockRole, customRequest.getHeader("role"));
+		assertEquals(mockUserName, customRequest.getHeader("userName"));
+	}
+
+	@Test
+	void validAuthHeaderWithDataAndErrorTest() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		MockFilterChain chain = Mockito.mock(MockFilterChain.class);
+		String mockRole = "ruolo", mockUserName = "user";
+
+		Algorithm algorithm = Algorithm.HMAC256(secret);
+		String token = JWT.create()
+				.withClaim("role", mockRole)
+				.withClaim("preferred_username", mockUserName)
+				.sign(algorithm);
+		request.addHeader("Authorization", token);
+
+		Mockito.doThrow(new IOException())
+				.when(chain)
+				.doFilter(Mockito.any(ServletRequest.class), Mockito.any(HttpServletResponse.class));
+
+		filter.doFilter(request, response, chain);
+
+		assertFalse(chain.getRequest() instanceof MutableHttpServletRequest);
 	}
 }
