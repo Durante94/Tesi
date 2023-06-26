cf56788114136e080338b089d1e1de8210f32066
Fabrizio Durante <fabrizio.durante@algowatt.com>
2023-06-26 23:12:24 +0200

    feat/gateway/tests

--- components/gateway/src/test/java/com/fabrizio/tesi/gateway/GatewayApplicationTests.java
+++ components/gateway/src/test/java/com/fabrizio/tesi/gateway/GatewayApplicationTests.java
@@ -2,8 +2,10 @@ package com.fabrizio.tesi.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:config/auth.properties")
 class GatewayApplicationTests {
 
 	@Test
--- components/gateway/src/test/java/com/fabrizio/tesi/gateway/config/ConfigControllerTest.java
+++ components/gateway/src/test/java/com/fabrizio/tesi/gateway/config/ConfigControllerTest.java
@@ -1,36 +1,71 @@
 package com.fabrizio.tesi.gateway.config;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.net.URI;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.http.server.reactive.MockServerHttpResponse;
 import org.springframework.mock.web.server.MockServerWebExchange;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.server.WebSession;
 import org.springframework.web.server.session.DefaultWebSessionManager;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest//(properties = {"keycloack.feingress=feIngrTest", "keycloack.oidcaddr=oidcTest"})
-@ActiveProfiles({"default", "auth"})
-// TODO: chiedere a chatgpt come importare tutti i file properties
+@SpringBootTest(properties = { "auth.enable=true" })
 public class ConfigControllerTest {
-    @Autowired
-    ConfigController controller;
+	@Autowired
+	ConfigController controller;
 
-    @Test
-    void destroySessionTest() {
-        MockServerHttpResponse response = new MockServerHttpResponse();
-        MockServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/"));
-        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-        WebSession session = sessionManager.getSession(exchange).block();
+	@Value("${keycloack.feingress}")
+	String entryPoint;
 
-        controller.destroySession(session, response);
+	@Test
+	void destroySessionTest() {
+		MockServerHttpResponse response = new MockServerHttpResponse();
+		MockServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/"));
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		WebSession session = sessionManager.getSession(exchange).block();
+		session.getAttributes().put("test", "to be canceled");
 
-        assertEquals(HttpStatus.FOUND, response.getStatusCode());
-        assertTrue(session.getAttributes().isEmpty());
-    }
+		controller.destroySession(session, response);
+
+		assertEquals(HttpStatus.FOUND, response.getStatusCode());
+		assertTrue(session.getAttributes().isEmpty());
+	}
+
+	@Test
+	void authenticatedTest() {
+		MockServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/"));
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		WebSession session = sessionManager.getSession(exchange).block();
+
+		ResponseEntity<Boolean> response = controller.authenticated(session);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+		assertFalse(response.getBody());
+	}
+
+	@Test
+	void reDoLoginTest() {
+		MockServerHttpResponse response = new MockServerHttpResponse();
+		MockServerWebExchange exchange = MockServerWebExchange.from(MockServerHttpRequest.get("/"));
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		WebSession session = sessionManager.getSession(exchange).block();
+
+		controller.reDoLogin(session, response);
+
+		assertEquals(HttpStatus.MOVED_PERMANENTLY, response.getStatusCode());
+		assertNotNull(response.getHeaders());
+		assertNotNull(response.getHeaders().getLocation());
+		assertEquals(URI.create(entryPoint + "/web/"), response.getHeaders().getLocation());
+	}
 }
