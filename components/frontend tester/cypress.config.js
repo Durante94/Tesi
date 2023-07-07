0864de04df2534b9e2a4808cb9e70ab603756161
Fabrizio Durante <fabrizio.durante@algowatt.com>
2023-07-07 23:30:42 +0200

    feat/cypress/creation

--- components/frontend tester/cypress.config.js
+++ components/frontend tester/cypress.config.js
@@ -4,9 +4,9 @@ module.exports = defineConfig({
   projectId: "9qb584",
   chromeWebSecurity: false,
   env: {
-    login_url: "http://localhost:30080/web/",
-    base_url: "http://localhost:30080",
-    keycloack_host: "https://localhost:30090",
+    login_url: "http://192.168.230.129:30080/web/",
+    base_url: "http://192.168.230.129:30080",
+    keycloack_host: "http://192.168.230.129:30090",
     username: "fabrizio",
     password: "fabrizio",
     doLogin: true
@@ -18,7 +18,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // experimentalSessionAndOrigin: true, //PERMETTE LA NAVIGAZIONE A UNA PAGINA CON ORIGINE DIVERSA
+    experimentalSessionAndOrigin: true, //PERMETTE LA NAVIGAZIONE A UNA PAGINA CON ORIGINE DIVERSA
     experimentalModifyObstructiveThirdPartyCode: true, // PERMETTE LA SCRITTURA SU PAGINA CON ORIGINE DIVERSA
     testIsolation: false //RETRCOMPATIBILITÀ, EVITIAMO CHE AD OGNI TEST CASE VENGA RESETTATO TUTTO (COOKIE, NAVIGAZIONE, ECC.)
   },
--- components/frontend tester/cypress/e2e/1-uiUnitTest.cy.js
+++ components/frontend tester/cypress/e2e/1-uiUnitTest.cy.js
@@ -1,4 +1,6 @@
-describe("Creation", () => {
+describe("Device Lifecycle", () => {
+    const devName = "cypressTest";
+
     before(() => {
         if (Cypress.env('doLogin'))
             cy.loginToKeycloack(
@@ -13,7 +15,36 @@ describe("Creation", () => {
         cy.wait('@userAdmin', 9000000000000000).should(xhr => { expect(xhr.response).to.have.property('statusCode', 200) });
     })
 
-    it("Compile the form", () => {
-        cy.contains("Add Device").should("exist").click();
+    it("Creation", () => {
+        cy.intercept({
+            method: 'GET',
+            url: '/api/agent',
+        }).as('apiAgent')
+        cy.intercept({
+            method: 'POST',
+            url: '/api/crud',
+        }).as('apiSave')
+
+        cy.get(".cp-add-dev").should("exist").click();
+
+        //CLICK SU SALVA TRIGGERA ERRORE CAMPI NON COMPILATI
+        cy.get(".cp-save").click();
+        cy.get(".cp-name").type(devName);
+        cy.get("cp-description").type("Creato con Cypress");
+        cy.get(".cp-agent").click();
+        cy.wait("@apiAgent", 9000000000000000).should(xhr => { expect(xhr.response).to.have.property('statusCode', 200) });
+        cy.get("").first().click();
+        cy.get(".cp-enable").click();
+
+        cy.get(".cp-amplitude").should("be.disabled").invoke("val").should("be.null");
+        cy.get(".cp-frequency").should("be.disabled").invoke("val").should("be.null");
+        cy.get(".cp-function").should("be.disabled").invoke("val").should("be.null");
+
+        cy.get(".cp-save").click();
+        cy.wait("@apiSave", 9000000000000000).should(xhr => { expect(xhr.response).to.have.property('statusCode', 200) });
+    });
+
+    it("Editing newly created device", () => {
+
     });
 });
